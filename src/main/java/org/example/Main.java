3ae2b6cec17ca9e4a6a1cf71c638a8af73380ee5
Marcello Feroce <ferocemarcello@gmail.com>
2022-07-15 08:05:11 +0200

    handling not found

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
+import java.util.Arrays;
+import java.util.List;
+
 import static spark.Spark.*;
 
 public class Main {
+    private static final List<String> paths = Arrays.asList("show_all","/hello", "/index");
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        port(8080);
+        get("/hello", (req, res) -> {
+            return "Hello World";
+        });
+        get("/index", (req, res) -> {
+            return "Index";
+        });
+        get("/show_all", (req, res) -> {
+            return "show_all";
+        });
+        notFound((req, res) -> {
+            res.type("text/html");
+            return "Page not found. Go to /index";
+        });
     }
 }
