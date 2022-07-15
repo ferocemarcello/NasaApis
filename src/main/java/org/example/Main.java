70dd0360df722d98e133473c9e37640ae1084d33
Marcello Feroce <ferocemarcello@gmail.com>
2022-07-15 08:14:02 +0200

    working on velocity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -8,8 +13,13 @@ public class Main {
     public static void main(String[] args) {
         port(8080);
         get("/hello", (req, res) -> {
-            return "Hello World";
-        });
+            Map<String, Object> model = new HashMap<>();
+            model.put("hello", "Velocity World");
+            model.put("person", new Person("Foobar"));
+            // The vm files are located under the resources directory
+            return new ModelAndView(model, "hello.vm");
+        }, new VelocityTemplateEngine());
+
         get("/index", (req, res) -> {
             return "Index";
         });
@@ -20,5 +30,19 @@ public class Main {
             res.type("text/html");
             return "Page not found. Go to /index";
         });
+    }
+    public static class Person {
+        private String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
